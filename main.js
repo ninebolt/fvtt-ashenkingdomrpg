9385beaacece963c8e36ce957b02f07198fdadd8
Ash Bhatnagar <ashbhatnagar7@gmail.com>
2020-09-23 21:44:47 -0400

    Started work on ability score calculations

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
+import { AKRPGActor } from './module/actor/actor.js';
+import { AKRPGActorSheet } from './module/actor/actor_sheet.js';
+
 Hooks.once('init', async function () {
+  console.log('AKRPG | System Loading');
+
+  game.boilerplate = {
+    AKRPGActor
+  };
+
+  CONFIG.Actor.entityClass = AKRPGActor;
+  
+  Actors.unregisterSheet('core', ActorSheet);
+  Actors.registerSheet('akrpg', AKRPGActorSheet, { types: ['character'], makeDefault: true });
+
   /**
    * Set an initiative formula for the system
    * @type {String}
---
+++ module/actor/actor.js
@@ -0,0 +1,48 @@
+export class AKRPGActor extends Actor {
+
+    /**
+     * @override
+     * This function initalizes the actor entity and calculates appropriate values.
+     */
+    prepareData() {
+        console.log('ACTOR DATA', this.data);
+        super.prepareData();
+
+        const actorData = this.data;
+        const data = actorData.data;
+        const flags = actorData.flags;
+
+        if (actorData.type === 'character') this._prepareCharacterData(actorData);
+    }
+
+    /**
+     * Prepares data for actor type 'character'
+     */
+    _prepareCharacterData() {
+        this._calculateAbilityScoreModifiers();
+        this._calculateSavingThrows();
+    }
+
+    /**
+     * Computes the ability score modifiers from base ability scores.
+     */
+    _calculateAbilityScoreModifiers() {
+        for (let ability of Object.values(this.data.data.abilities)) {
+            if (ability.value) ability.modifier = Math.floor((ability.value - 10) / 2);
+            console.log('ABILITY', ability);
+        }
+    }
+
+    /**
+     * Compuates the saving throw totals from the character's base, relevant ability score, and misc. modifiers.
+     */
+    _calculateSavingThrows() {
+        for (let savingThrow of Object.values(this.data.data.savingThrows)) {
+            if (!savingThrow.base) savingThrow.base = 0;
+            if (!savingThrow.miscMod) savingThrow.miscMod = 0;
+
+            savingThrow.value = savingThrow.base + this.data.data.abilities[savingThrow.baseAbility].mod + savingThrow.miscMod;
+            console.log('SAVING THROW', savingThrow);
+        }
+    }
+}
